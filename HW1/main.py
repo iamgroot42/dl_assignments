a0f7d943a38c5f47bff2f7efb25ce23ff0bb8cb0
Anshuman Suri <anshuman14021@iiitd.ac.in>
2017-01-22 22:01:26 +0530

    Implemented basic methods for sigmoid, leru, add,mul.

---
+++ HW1/main.py
@@ -0,0 +1,7 @@
+# from keras.datasets import mnist
+import numpy as np
+import neurons
+
+if __name__ == "__main__":
+	# (X_train, y_train), (X_test, y_test) = mnist.load_data()
+	x = neurons.Multiply()
