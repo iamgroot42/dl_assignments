ac95ac99b3cb61ffb992ec4ac73f974d38fada2c
Anshuman Suri <anshuman14021@iiitd.ac.in>
2017-02-25 15:02:23 +0530

    Train/test split in data, flatten scans to 2d images

--- HW2/read_data.py
+++ HW2/read_data.py
@@ -2,10 +2,29 @@ import h5py
 import numpy as np
 
 
-def get_data(base_dir='Data/'):
+def get_data(base_dir='Data/', splice=False):
 	train_data = h5py.File(base_dir + 'train_540k.mat').get('training')[0]
-	train_data = train_data.reshape(13,13,13,540000)
+	if splice:
+		train_data = train_data.reshape(13, 13, 13*540000)
+	else:
+		train_data = train_data.reshape(13, 13, 13, 540000)
 	return train_data
+
+
+def split_data(X, y, split=0.8):
+	train_examples = X.shape[-1] * split
+	if len(X.shape) == 3:
+		X_train = X[:,:,:train_examples]
+		X_test = X[:,:,train_examples:]
+		y = np.repeat(y, 13, 0)
+		y_train = y[:,:train_examples]
+		y_test = y[:,train_examples:]
+	else:
+		X_train = X[:,:,:,:train_examples]
+		X_test = X[:,:,:,train_examples:]
+		y_train = y[:,:train_examples]
+		y_test = y[:,train_examples:]
+	return X_train, y_train, X_test, y_test
 
 
 def get_labels(base_dir='Data/'):
@@ -16,6 +35,12 @@ def get_labels(base_dir='Data/'):
 	one_hot = np.array([label_csf, label_wm, label_gm])
 	one_hot = np.swapaxes(one_hot, 0, 1)
 	return one_hot
+
+
+def load_data(base_dir='Data/', split=0.8):
+	X = get_data()
+	y = get_labels()
+	return split_data(X, y, split)
 
 
 if __name__ == "__main__":
