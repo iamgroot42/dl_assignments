2fceea490538fb40cabddf560eb6ba1e59730f31
Anshuman Suri <anshuman14021@iiitd.ac.in>
2017-03-04 11:26:00 +0530

    Adding weak model

--- HW2/main.py
+++ HW2/main.py
@@ -4,10 +4,12 @@ tf.python.control_flow_ops = tf
 
 from tensorflow.python.platform import flags
 from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
 
 import read_data
 import classify_cnn
 import segment_cnn
+import segmentweak_cnn
 
 FLAGS = flags.FLAGS
 
@@ -16,6 +18,7 @@ flags.DEFINE_integer('batch_size', 128, 'Batch size')
 flags.DEFINE_float('learning_rate', 0.1, 'Learning rate for training')
 flags.DEFINE_string('type', 'segment', 'Classification or segmentation')
 flags.DEFINE_integer('dimension', 2, '2D or 3D')
+flags.DEFINE_string('model_path', "", 'Path to saved model with weights')
 
 
 def load_data(ttype, dimension):
@@ -42,28 +45,36 @@ def get_model(ttype, dimension):
 	else:
 		# Ideal learning rate: 0.01
 		if dimension is 2:
-			model = segment_cnn.CNN2D(learning_rate=FLAGS.learning_rate)
+			model = segmentweak_cnn.CNN2D(learning_rate=FLAGS.learning_rate)
 		else:
-			model = segment_cnn.CNN3D(learning_rate=FLAGS.learning_rate)
+			model = segmentweak_cnn.CNN3D(learning_rate=FLAGS.learning_rate)
 	return model
+
+
+def test_model(model, X, y):
+	accuracy, f_score = model.evaluate(X,y)[1:]
+	print "\nTesting accuracy:",accuracy*100,"%"
+	print "F-Score accuracy:",f_score	
 
 
 if __name__ == "__main__":
 	xtr, ytr, xte, yte, weights = load_data(FLAGS.type, FLAGS.dimension)
-	model = get_model(FLAGS.type, FLAGS.dimension)
-	if FLAGS.type == "classify":
-		model.fit(xtr, ytr,
-			nb_epoch=FLAGS.nb_epochs,
-			batch_size=FLAGS.batch_size,
-			validation_split=0.2,
-			class_weight=weights,
-			callbacks=[ModelCheckpoint("Models/" + FLAGS.type + str(FLAGS.dimension) + ".{epoch:02d}-{val_acc:.2f}.hdf5")])
+	if FLAGS.model_path == "":
+		model = get_model(FLAGS.type, FLAGS.dimension)
+		if FLAGS.type == "classify":
+			model.fit(xtr, ytr,
+				nb_epoch=FLAGS.nb_epochs,
+				batch_size=FLAGS.batch_size,
+				validation_split=0.2,
+				class_weight=weights,
+				callbacks=[ModelCheckpoint("Models/" + FLAGS.type + str(FLAGS.dimension) + ".{epoch:02d}-{val_acc:.2f}.hdf5")])
+		else:
+			model.fit(xtr, ytr,
+				nb_epoch=FLAGS.nb_epochs,
+				batch_size=FLAGS.batch_size,
+				validation_split=0.2,
+				callbacks=[ModelCheckpoint("Models/" + FLAGS.type + str(FLAGS.dimension) + ".{epoch:02d}-{val_acc:.2f}.hdf5")])
 	else:
-		model.fit(xtr, ytr,
-		nb_epoch=FLAGS.nb_epochs,
-		batch_size=FLAGS.batch_size,
-		validation_split=0.2,
-		callbacks=[ModelCheckpoint("Models/" + FLAGS.type + str(FLAGS.dimension) + ".{epoch:02d}-{val_acc:.2f}.hdf5")])
-	accuracy, f_score = model.evaluate(xte, yte)
-	print "\nTesting accuracy:",accuracy*100,"%"
-	print "\nF-Score accuracy:",f_score,"%"
+		model = load_model(FLAGS.model_path)
+	test_model(model, xte, yte)
+
---
+++ HW2/segmentweak_cnn.py
@@ -0,0 +1,133 @@
+import numpy as np
+
+from keras.layers import BatchNormalization
+from keras.layers import Convolution2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
+from keras.layers import Convolution3D, MaxPooling3D, UpSampling3D, ZeroPadding3D
+from keras.layers import Dense, Flatten, Input, Dropout, Activation, Reshape, Permute
+from keras.models import Sequential
+from keras.optimizers import Adadelta
+
+
+def addConv_BatchNorm2d(size, x, y, model, addInput = False):
+	if (addInput):
+		model.add(Convolution2D(size, x, y, border_mode = 'valid', input_shape = (1, 256, 256)))
+	else:
+		model.add(Convolution2D(size, x, y, border_mode = 'valid'))
+	model.add(BatchNormalization())
+	model.add(Activation('relu'))
+
+
+def addConv_BatchNorm3d(size, x, y, z, model, addInput = False):
+	if (addInput):
+		model.add(Convolution3D(size, x, y, z, border_mode = 'valid', input_shape = (1, 256, 256, 128)))
+	else:
+		model.add(Convolution3D(size, x, y, z, border_mode = 'valid'))
+	model.add(BatchNormalization())
+	model.add(Activation('relu'))
+
+
+def CNN2D(n_labels=4, learning_rate=0.01, pad=1):
+	model = Sequential()
+
+	model.add(ZeroPadding2D(padding=(pad,pad), input_shape=(1, 256, 256)))
+	addConv_BatchNorm2d(64, 3, 3, model)
+	model.add(MaxPooling2D())
+	
+	model.add(ZeroPadding2D(padding=(pad,pad)))
+	addConv_BatchNorm2d(128, 3, 3, model)
+	model.add(MaxPooling2D())
+	
+	model.add(ZeroPadding2D(padding=(pad,pad)))
+	addConv_BatchNorm2d(256, 3, 3, model)
+	model.add(MaxPooling2D())
+
+	model.add(ZeroPadding2D(padding=(pad,pad)))
+	addConv_BatchNorm2d(512, 3, 3, model)
+	
+	# Encoding done 
+	encoder = model
+
+	model.add(ZeroPadding2D(padding=(pad,pad)))
+	model.add(Convolution2D(512, 3, 3, border_mode='valid'))
+	model.add(BatchNormalization())
+
+	model.add(UpSampling2D())
+	model.add(ZeroPadding2D(padding=(pad,pad)))
+	model.add(Convolution2D(256, 3, 3, border_mode='valid'))
+	model.add(BatchNormalization())
+
+	model.add(UpSampling2D())
+	model.add(ZeroPadding2D(padding=(pad,pad)))
+	model.add(Convolution2D(128, 3, 3, border_mode='valid'))
+	model.add(BatchNormalization())
+
+	model.add(UpSampling2D())
+	model.add(ZeroPadding2D(padding=(pad,pad)))
+	model.add(Convolution2D(64, 3, 3, border_mode='valid'))
+	model.add(BatchNormalization())
+	
+	model.add(Convolution2D(n_labels, 1, 1, border_mode='valid'))
+	model.add(Reshape((n_labels, 256 * 256)))
+	model.add(Permute((2, 1)))
+	model.add(Activation('softmax'))
+
+	model.compile(loss = 'categorical_crossentropy', optimizer = Adadelta(lr = learning_rate), metrics = ['accuracy','fmeasure'])
+	return model
+
+
+def CNN3D(n_labels=4, learning_rate=0.01, pad=1):
+	model = Sequential()
+
+	model.add(ZeroPadding3D(padding=(pad,pad,pad), input_shape=(1,256,256,128)))
+	addConv_BatchNorm3d(16, 3, 3, 3, model)
+	addConv_BatchNorm3d(16, 3, 3, 3, model)
+	model.add(MaxPooling3D())
+	
+	model.add(ZeroPadding3D(padding=(pad,pad,pad)))
+	addConv_BatchNorm3d(32, 3, 3, 3, model)
+	addConv_BatchNorm3d(32, 3, 3, 3, model)
+	model.add(MaxPooling3D())
+	
+	model.add(ZeroPadding3D(padding=(pad,pad,pad)))
+	addConv_BatchNorm3d(64, 3, 3, 3, model)
+	addConv_BatchNorm3d(64, 3, 3, 3, model)
+	
+	# Encoding done 
+	encoder = model
+
+	model.add(ZeroPadding3D(padding=(pad,pad,pad)))
+	model.add(Convolution3D(64, 3, 3, 3, border_mode='valid'))
+	model.add(BatchNormalization())
+
+	model.add(ZeroPadding3D(padding=(pad,pad,pad)))
+	model.add(Convolution3D(64, 3, 3, 3, border_mode='valid'))
+	model.add(BatchNormalization())
+
+	model.add(UpSampling3D())
+	model.add(ZeroPadding3D(padding=(pad,pad,pad)))
+	model.add(Convolution3D(32, 3, 3, 3, border_mode='valid'))
+	model.add(BatchNormalization())
+	
+	model.add(UpSampling3D())
+	model.add(ZeroPadding3D(padding=(pad,pad,pad)))
+	model.add(Convolution3D(32, 3, 3, 3, border_mode='valid'))
+	model.add(BatchNormalization())
+
+	model.add(UpSampling3D())
+	model.add(ZeroPadding3D(padding=(pad,pad,pad)))
+	model.add(Convolution3D(16, 3, 3, 3,border_mode='valid'))
+	model.add(BatchNormalization())
+
+	model.add(UpSampling3D())
+	model.add(ZeroPadding3D(padding=(pad,pad,pad)))
+	model.add(Convolution3D(16, 3, 3, 3, border_mode='valid'))
+	model.add(BatchNormalization())
+	
+	model.add(Convolution3D(n_labels, 1, 1, 1,border_mode='valid'))
+	model.add(Reshape((n_labels, 256 * 256 * 128)))
+	model.add(Permute((2, 1)))
+	model.add(Activation('softmax'))
+
+	model.compile(loss = 'categorical_crossentropy', optimizer = Adadelta(lr = learning_rate), metrics = ['accuracy','fmeasure'])
+	return model
+
